05192254918b23660757bfefbf972f359e74f910
bunder2015 <omfgbunder@gmail.com>
2019-06-07 03:50:25 -0400

    more code
    Signed-off-by: bunder2015 <omfgbunder@gmail.com>

--- boilerplate.asm
+++ boilerplate.asm
@@ -28,9 +28,9 @@
 	; Zero-page memory $0000-00FF
 	.zp
 
-JOY1INPUT:
+JOY1IN:
 	.ds 1
-JOY2INPUT:
+JOY2IN:
 	.ds 1
 
 	; Debugging
@@ -64,6 +64,7 @@ DBGSP:
 
 MAIN:
 	;; TODO
+
 	JMP MAIN
 
 ;;;;;;;;;;
@@ -72,6 +73,71 @@ MAIN:
 	.org $E000
 
 	;; TODO
+
+LOADPALS:
+	LDA PPUSTATUS		; Read PPU status to reset PPUADDR latch
+	LDA #$3F
+	STA PPUADDR
+	LDA #$00
+	STA PPUADDR		; Set PPUADDR to $3F00
+
+	LDX #$00
+
+.PALSLOOP:
+	LDA PALETTES, X		; Read palette colour
+	STA PPUDATA		; Store to PPU
+	INX
+	CPX #$20
+	BNE .PALSLOOP		; Loop through 4 BG and SPR palettes
+
+	RTS
+
+READJOY1:
+	LDA #$01
+	STA STROBE		; Bring strobe latch high
+	LDA #$00
+	STA STROBE		; Bring strobe latch low
+
+	LDX #$08
+
+.JOY1LOOP:
+	LDA JOY1		; Read Joypad 1
+	LSR A			; Shift bit into carry
+	ROL <JOY1IN		; Rotate carry into storage
+	DEX
+	BNE .JOY1LOOP
+
+	RTS
+
+READJOY2:
+	LDA #$01
+	STA STROBE		; Bring strobe latch high
+	LDA #$00
+	STA STROBE		; Bring strobe latch low
+
+	LDX #$08
+
+.JOY2LOOP:
+	LDA JOY2		; Read Joypad 2
+	LSR A			; Shift bit into carry
+	ROL <JOY2IN		; Rotate carry into storage
+	DEX
+	BNE .JOY2LOOP
+
+	RTS
+
+	.org $F000
+
+PALETTES:
+	.db $0F,$20,$10,$00	; BG palette 0
+	.db $0F,$20,$10,$00	; BG palette 1
+	.db $0F,$20,$10,$00	; BG palette 2
+	.db $0F,$20,$10,$00	; BG palette 3
+
+	.db $0F,$20,$10,$00	; SPR palette 0
+	.db $0F,$20,$10,$00	; SPR palette 1
+	.db $0F,$20,$10,$00	; SPR palette 2
+	.db $0F,$20,$10,$00	; SPR palette 3
 
 	.org $FF00
 
@@ -111,16 +177,42 @@ VB2:
 	BPL VB2			; Wait for second vblank
 
 RESETDONE:
+	JSR LOADPALS		; Load palettes
+
 	LDA #$00
 	LDX #$00
-	CLV
+	CLV			; Reset registers
 	JMP MAIN		; Go to main code loop
 
 IRQ:
 	;; TODO
 
+	RTI			; Exit IRQ
+
 NMI:
+	PHA
+	TXA
+	PHA
+	TYA
+	PHA			; Push A/X/Y onto the stack
+
 	;; TODO
+
+	LDA #$00
+	STA OAMADDR
+	LDA #$02
+	STA OAMDMA		; DMA transfer $0200-$02FF to PPU OAM
+
+	JSR READJOY1		; Read controller 1
+	JSR READJOY2		; Read controller 2
+
+	PLA
+	TAY
+	PLA
+	TAX
+	PLA			; Pull A/X/Y from the stack
+
+	RTI			; Exit NMI
 
 ;;;;;;;;;;
 
